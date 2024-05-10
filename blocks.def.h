6069dc9ce9351139934c775b69fc51f1ce79b721
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-10 10:54:17 +0100

    Explicitly cast to `char*`

--- blocks.def.h
+++ blocks.def.h
@@ -6,9 +6,9 @@ typedef struct {
 } Block;
 
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	0},
-	{"󰥔 ", "date '+%b %d (%a) %I:%M%p'", 5,	0},
+	/*Icon*/  /*Command*/  /*Update Interval*/  /*Update Signal*/
+	{(char*)" ", (char*)"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	0},
+	{(char*)"󰥔 ", (char*)"date '+%b %d (%a) %I:%M%p'", 5,	0},
 };
 
 // `\0` means no delimiter.
