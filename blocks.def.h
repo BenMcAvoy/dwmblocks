973850e310e8de9714f37820a961114559a00cfe
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-10 10:42:43 +0100

    Create configuration for editors

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,16 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+typedef struct {
+    char *icon;
+    char *command;
+    unsigned int interval;
+    unsigned int signal;
+} Block;
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	0},
+	{"󰥔 ", "date '+%b %d (%a) %I:%M%p'", 5,	0},
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+// `\0` means no delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
